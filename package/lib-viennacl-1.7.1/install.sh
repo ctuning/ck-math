110ebd36625ffa5168fa859c075876b3d617d3a0
Anton Lokhmotov <anton@dividiti.com>
2016-06-23 11:50:23 +0000

    Disable ViennaCL tests and examples.

--- package/lib-viennacl-1.7.1/install.sh
+++ package/lib-viennacl-1.7.1/install.sh
@@ -17,60 +17,76 @@ export VIENNACL_SRC_DIR=${INSTALL_DIR}/src
 export VIENNACL_OBJ_DIR=${INSTALL_DIR}/obj
 export VIENNACL_LIB_DIR=${INSTALL_DIR}/lib
 
+################################################################################
 echo ""
 echo "Cloning ViennaCL from '${VIENNACL_URL}' ..."
+
 rm -rf ${VIENNACL_SRC_DIR}
+
 git clone ${VIENNACL_URL} --no-checkout ${VIENNACL_SRC_DIR}
+
 if [ "${?}" != "0" ] ; then
   echo "Error: Cloning ViennaCL from '${VIENNACL_URL}' failed!"
   exit 1
 fi
 
+################################################################################
 echo ""
 echo "Checking out the '${VIENNACL_TAG}' release of ViennaCL ..."
+
 cd ${VIENNACL_SRC_DIR}
+
 git checkout tags/${VIENNACL_TAG} -b ${VIENNACL_TAG}
+
 if [ "${?}" != "0" ] ; then
   echo "Error: Checking out the '${VIENNACL_TAG}' release of ViennaCL failed!"
   exit 1
 fi
 
+################################################################################
 echo ""
 echo "Configuring ViennaCL..."
 
-rm -rf $VIENNACL_OBJ_DIR
-mkdir  $VIENNACL_OBJ_DIR
-cd $VIENNACL_OBJ_DIR
+rm -rf ${VIENNACL_OBJ_DIR}
+mkdir  ${VIENNACL_OBJ_DIR}
+cd ${VIENNACL_OBJ_DIR}
 
-#-DBOOSTPATH=xyz
-cmake ../src -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+cmake ${VIENNACL_SRC_DIR} \
+  -DBUILD_TESTING=OFF \
+  -DBUILD_EXAMPLES=OFF \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+#TODO: -DBOOSTPATH=xyz ?
+
 if [ "$?" != "0" ]; then
   echo "Error: failed configuring ViennaCL ..."
   read -p "Press any key to continue!"
   exit $?
 fi
 
+################################################################################
 echo ""
 echo "Building ViennaCL..."
 
-cmake --build .
-#make
+cmake --build ${VIENNACL_OBJ_DIR}
+#make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS}
+
 if [ "$?" != "0" ]; then
   echo "Error: failed making ViennaCL ..."
   read -p "Press any key to continue!"
   exit $?
 fi
 
+################################################################################
 echo ""
 echo "Installing ViennaCL..."
 
 cmake -P cmake_install.cmake
 #make install
 
-# Somehow the library was not copied via the above command.
-# Doing it manually for now...
-mkdir -p $VIENNACL_LIB_DIR
-cp -f $VIENNACL_OBJ_DIR/libviennacl/libviennacl.so $VIENNACL_LIB_DIR
+# Weirdly, the above command does not copy the library, so doing this manually...
+mkdir -p ${VIENNACL_LIB_DIR}
+cp -f ${VIENNACL_OBJ_DIR}/libviennacl/libviennacl.so ${VIENNACL_LIB_DIR}
+
 if [ "$?" != "0" ]; then
   echo "Error: failed installing ViennaCL ..."
   read -p "Press any key to continue!"
