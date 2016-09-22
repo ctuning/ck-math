c18464a4c3219d3815a234e0f4f9d99b08940729
Anton Lokhmotov <anton@dividiti.com>
2016-09-22 13:40:03 +0000

    Add build and test log. Test failure is warning, not error.

--- package/lib-clblast-trunk-test-vs-clblas/install.sh
+++ package/lib-clblast-trunk-test-vs-clblas/install.sh
@@ -16,6 +16,8 @@
 
 export CLBLAST_SRC_DIR=${INSTALL_DIR}/src
 export CLBLAST_BLD_DIR=${INSTALL_DIR}/bld
+export CLBLAST_TST_LOG=${INSTALL_DIR}/tst.log
+export CLBLAST_BLD_LOG=${INSTALL_DIR}/bld.log
 
 export CLBLAST_PATCH=${PACKAGE_DIR}/test_correctness_common_ref_includes.patch
 
@@ -57,9 +59,20 @@ fi
 ################################################################################
 echo ""
 echo "Building the '${CLBLAST_BRANCH}' branch of CLBlast ..."
+echo "Logging into '${CLBLAST_BLD_LOG}' ..."
+touch ${CLBLAST_BLD_LOG}
+
+echo "** DATE **" >> ${CLBLAST_BLD_LOG}
+date >> ${CLBLAST_BLD_LOG}
+
+echo "** SET **" >> ${CLBLAST_BLD_LOG}
+set >> ${CLBLAST_BLD_LOG}
+
 rm -rf ${CLBLAST_BLD_DIR}
 mkdir -p ${CLBLAST_BLD_DIR}
 cd ${CLBLAST_BLD_DIR}
+
+echo "** CMAKE **" >> ${CLBLAST_BLD_LOG}
 cmake ${CLBLAST_SRC_DIR} \
   -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
   -DCMAKE_C_COMPILER=${CK_CC} -DCMAKE_CXX_COMPILER=${CK_CXX} \
@@ -68,19 +81,27 @@ cmake ${CLBLAST_SRC_DIR} \
   -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
   -DTESTS=ON \
   -DCLBLAS_INCLUDE_DIRS:PATH=${CK_ENV_LIB_CLBLAS_INCLUDE} \
-  -DCLBLAS_LIBRARIES:FILEPATH=${CK_ENV_LIB_CLBLAS_LIB}/${CK_ENV_LIB_CLBLAS_DYNAMIC_NAME}
+  -DCLBLAS_LIBRARIES:FILEPATH=${CK_ENV_LIB_CLBLAS_LIB}/${CK_ENV_LIB_CLBLAS_DYNAMIC_NAME} \
+  >>${CLBLAST_BLD_LOG} 2>&1
 
-make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS}
+echo "** MAKE **" >> ${CLBLAST_BLD_LOG}
+make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} >>${CLBLAST_BLD_LOG} 2>&1
 if [ "${?}" != "0" ] ; then
   echo "Error: Building the '${CLBLAST_BRANCH}' branch of CLBlast failed!"
   exit 1
 fi
 
+###############################################################################
+echo ""
+echo "Installed the '${CLBLAST_BRANCH}' branch of CLBlast into '${INSTALL_DIR}'."
+
 ################################################################################
 echo ""
 echo "Testing the '${CLBLAST_BRANCH}' branch of CLBlast ..."
-make alltests
+echo "Logging into '${CLBLAST_TST_LOG}' ..."
+touch ${CLBLAST_TST_LOG}
+
+make alltests >>${CLBLAST_TST_LOG} 2>&1
 if [ "${?}" != "0" ] ; then
-  echo "Error: Testing the '${CLBLAST_BRANCH}' branch of CLBlast failed!"
-  exit 1
+  echo "Warning: Testing the '${CLBLAST_BRANCH}' branch of CLBlast failed!"
 fi
