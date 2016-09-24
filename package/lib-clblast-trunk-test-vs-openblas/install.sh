a14b5c282181b1ea9bb234b82314de1f5257b8a0
Anton Lokhmotov <anton@dividiti.com>
2016-09-24 13:18:40 +0000

    Rename CLBlast vs OpenBLAS patch to match PR101 (+ install.sh).

--- package/lib-clblast-trunk-test-vs-openblas/install.sh
+++ package/lib-clblast-trunk-test-vs-openblas/install.sh
@@ -19,12 +19,12 @@ export CLBLAST_BLD_DIR=${INSTALL_DIR}/bld
 export CLBLAST_TST_LOG=${INSTALL_DIR}/tst.log
 export CLBLAST_BLD_LOG=${INSTALL_DIR}/bld.log
 
-export CLBLAST_PATCH=${PACKAGE_DIR}/test_correctness_common_ref_includes.patch
+# NB: List of pending patches (PRs or otherwise) to this CLBlast branch.
+export CLBLAST_PR101_PATCH=${PACKAGE_DIR}/clblast-pr101.patch
 
 # Must be set before running CMake.
-# TODO: Set to CK environment.
-export CLBLAST_PLATFORM=0
-export CLBLAST_DEVICE=0
+export CLBLAST_PLATFORM=${CK_COMPUTE_PLATFORM_ID}
+export CLBLAST_DEVICE=${CK_COMPUTE_DEVICE_ID}
 
 ################################################################################
 echo ""
@@ -50,7 +50,7 @@ fi
 echo ""
 echo "Patching the '${CLBLAST_BRANCH}' branch of CLBlast ..."
 cd ${CLBLAST_SRC_DIR}
-patch -p1 < ${CLBLAST_PATCH}
+patch -p1 < ${CLBLAST_PR101_PATCH}
 if [ "${?}" != "0" ] ; then
   echo "Error: Patching the '${CLBLAST_BRANCH}' branch of CLBlast failed!"
   exit 1
