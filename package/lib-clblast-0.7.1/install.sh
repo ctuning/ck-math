8d2b7177500a3c1bffba9e8b06f2e6e05303a05c
Anton Lokhmotov <anton@dividiti.com>
2016-09-26 19:42:23 +0100

    First create build dir; then touch log file.

--- package/lib-clblast-0.7.1/install.sh
+++ package/lib-clblast-0.7.1/install.sh
@@ -42,6 +42,8 @@ fi
 ################################################################################
 echo
 echo "Logging into '${CLBLAST_BLD_LOG}' ..."
+
+rm -rf ${CLBLAST_BLD_DIR}; mkdir ${CLBLAST_BLD_DIR}
 touch ${CLBLAST_BLD_LOG}; sleep 0.5
 
 echo "** DATE **" >> ${CLBLAST_BLD_LOG}
@@ -55,10 +57,7 @@ echo ""
 echo "Configuring the '${CLBLAST_TAG}' release of CLBlast ..."
 echo "** CMAKE **" >> ${CLBLAST_BLD_LOG}
 
-rm -rf ${CLBLAST_BLD_DIR}
-mkdir -p ${CLBLAST_BLD_DIR}
 cd ${CLBLAST_BLD_DIR}
-
 cmake ${CLBLAST_SRC_DIR} \
   -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
   -DCMAKE_C_COMPILER=${CK_CC} -DCMAKE_CXX_COMPILER=${CK_CXX} \
@@ -66,12 +65,17 @@ cmake ${CLBLAST_SRC_DIR} \
   -DOPENCL_INCLUDE_DIRS:PATH=${CK_ENV_LIB_OPENCL_INCLUDE} \
   -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
   >>${CLBLAST_BLD_LOG} 2>&1
+if [ "${?}" != "0" ] ; then
+  echo "Error: Configuring the '${CLBLAST_TAG}' release of CLBlast failed!"
+  exit 1
+fi
 
 ################################################################################
 echo ""
 echo "Building the '${CLBLAST_TAG}' release of CLBlast ..."
 echo "** MAKE **" >> ${CLBLAST_BLD_LOG}
 
+cd ${CLBLAST_BLD_DIR}
 make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} install >>${CLBLAST_BLD_LOG} 2>&1
 if [ "${?}" != "0" ] ; then
   echo "Error: Building the '${CLBLAST_TAG}' release of CLBlast failed!"
@@ -83,6 +87,7 @@ echo ""
 echo "Installing the '${CLBLAST_TAG}' release of CLBlast ..."
 echo "** INSTALL **" >> ${CLBLAST_BLD_LOG}
 
+cd ${CLBLAST_BLD_DIR}
 make install >>${CLBLAST_BLD_LOG} 2>&1
 if [ "${?}" != "0" ] ; then
   echo "Error: Installing the '${CLBLAST_TAG}' release of CLBlast failed!"
