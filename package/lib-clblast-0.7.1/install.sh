e6eed60ed0f20838034e5eff26794faba35bf6b3
Anton Lokhmotov <anton@dividiti.com>
2016-09-26 16:23:06 +0000

    Update CLBlast install.sh script similar to OpenBLAS.

--- package/lib-clblast-0.7.1/install.sh
+++ package/lib-clblast-0.7.1/install.sh
@@ -3,8 +3,8 @@
 #
 # Installation script for CLBlast.
 #
-# See CK LICENSE for licensing details.
-# See CK COPYRIGHT for copyright details.
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
 #
 # Developer(s):
 # - Grigori Fursin, 2015;
@@ -15,10 +15,13 @@
 # INSTALL_DIR
 
 export CLBLAST_SRC_DIR=${INSTALL_DIR}/src
-export CLBLAST_OBJ_DIR=${INSTALL_DIR}/obj
+export CLBLAST_BLD_DIR=${INSTALL_DIR}/bld
+export CLBLAST_BLD_LOG=${CLBLAST_BLD_DIR}/build.log
 
+################################################################################
 echo ""
 echo "Cloning CLBlast from '${CLBLAST_URL}' ..."
+
 rm -rf ${CLBLAST_SRC_DIR}
 git clone ${CLBLAST_URL} --no-checkout ${CLBLAST_SRC_DIR}
 if [ "${?}" != "0" ] ; then
@@ -26,6 +29,7 @@ if [ "${?}" != "0" ] ; then
   exit 1
 fi
 
+################################################################################
 echo ""
 echo "Checking out the '${CLBLAST_TAG}' release of CLBlast ..."
 cd ${CLBLAST_SRC_DIR}
@@ -35,19 +39,56 @@ if [ "${?}" != "0" ] ; then
   exit 1
 fi
 
+################################################################################
+echo
+echo "Logging into '${CLBLAST_BLD_LOG}' ..."
+touch ${CLBLAST_BLD_LOG}
+
+echo "** DATE **" >> ${CLBLAST_BLD_LOG}
+date >> ${CLBLAST_BLD_LOG}
+
+echo "** SET **" >> ${CLBLAST_BLD_LOG}
+set >> ${CLBLAST_BLD_LOG}
+
+################################################################################
 echo ""
-echo "Building the '${CLBLAST_TAG}' release of CLBlast ..."
-rm -rf ${CLBLAST_OBJ_DIR}
-mkdir -p ${CLBLAST_OBJ_DIR}
-cd ${CLBLAST_OBJ_DIR}
+echo "Configuring the '${CLBLAST_TAG}' release of CLBlast ..."
+echo "** CMAKE **" >> ${CLBLAST_BLD_LOG}
+
+rm -rf ${CLBLAST_BLD_DIR}
+mkdir -p ${CLBLAST_BLD_DIR}
+cd ${CLBLAST_BLD_DIR}
+
 cmake ${CLBLAST_SRC_DIR} \
   -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
   -DCMAKE_C_COMPILER=${CK_CC} -DCMAKE_CXX_COMPILER=${CK_CXX} \
   -DOPENCL_LIBRARIES:FILEPATH=${CK_ENV_LIB_OPENCL_LIB}/${CK_ENV_LIB_OPENCL_DYNAMIC_NAME} \
   -DOPENCL_INCLUDE_DIRS:PATH=${CK_ENV_LIB_OPENCL_INCLUDE} \
-  -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}
-make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} install
+  -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
+  >>${CLBLAST_BLD_LOG} 2>&1
+
+################################################################################
+echo ""
+echo "Building the '${CLBLAST_TAG}' release of CLBlast ..."
+echo "** MAKE **" >> ${CLBLAST_BLD_LOG}
+
+make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} install >>${CLBLAST_BLD_LOG} 2>&1
 if [ "${?}" != "0" ] ; then
   echo "Error: Building the '${CLBLAST_TAG}' release of CLBlast failed!"
   exit 1
 fi
+
+################################################################################
+echo ""
+echo "Installing the '${CLBLAST_TAG}' release of CLBlast ..."
+echo "** INSTALL **" >> ${CLBLAST_BLD_LOG}
+
+make install >>${CLBLAST_BLD_LOG} 2>&1
+if [ "${?}" != "0" ] ; then
+  echo "Error: Installing the '${CLBLAST_TAG}' release of CLBlast failed!"
+  exit 1
+fi
+
+###############################################################################
+echo ""
+echo "Installed CLBlast ${CLBLAST_TAG} into '${INSTALL_DIR}'."
