9c39c2fdf985238ed616bdd4ed2184aa3838d63b
Anton Lokhmotov <anton@dividiti.com>
2016-09-26 11:26:51 +0100

    Add CLBlast vs clBLAS patch for #PR103 (not created yet).

--- package/lib-clblast-trunk-test-vs-clblas/install.sh
+++ package/lib-clblast-trunk-test-vs-clblas/install.sh
@@ -21,7 +21,7 @@ export CLBLAST_BLD_LOG=${INSTALL_DIR}/bld.log
 
 # NB: List of pending patches (PRs or otherwise) to this CLBlast branch.
 # When no pending patches, comment out the patch command below.
-#export CLBLAST_PR101_PATCH=${PACKAGE_DIR}/clblast-pr101.patch
+export CLBLAST_PR103_PATCH=${PACKAGE_DIR}/clblast-pr103.patch
 
 # NB: Must be set before running CMake.
 export CLBLAST_PLATFORM=${CK_COMPUTE_PLATFORM_ID}
@@ -51,7 +51,7 @@ fi
 echo ""
 echo "Patching the '${CLBLAST_BRANCH}' branch of CLBlast ..."
 cd ${CLBLAST_SRC_DIR}
-#patch -p1 < ${CLBLAST_PR101_PATCH}
+patch -p1 < ${CLBLAST_PR103_PATCH}
 if [ "${?}" != "0" ] ; then
   echo "Error: Patching the '${CLBLAST_BRANCH}' branch of CLBlast failed!"
   exit 1
