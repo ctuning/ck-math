040c1e90e15c0ff616e0e1de45e3c96ba4c4abe4
Grigori Fursin <grigori.fursin@cTuning.org>
2017-01-19 22:08:03 +0100

    added support to compile CLBlast on Windows for Windows and Android ...

--- package/lib-clblast-master-universal/scripts.android/install.sh
+++ package/lib-clblast-master-universal/scripts.android/install.sh
@@ -10,11 +10,6 @@
 # - Grigori Fursin, grigori.fursin@cTuning.org, 2017
 #
 
-CK_OPENMP=ON
-if [ "${CK_HAS_OPENMP}" = "0"  ]; then
-  CK_OPENMP=OFF
-fi
-
 export CK_CMAKE_EXTRA="${CK_CMAKE_EXTRA} \
  -DOPENCL_ROOT=${CK_ENV_LIB_OPENCL} \
  -DOPENCL_LIBRARIES=${CK_ENV_LIB_OPENCL_LIB}/libOpenCL.so \
