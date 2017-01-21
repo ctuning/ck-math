c89c88ccc508941b35d3ffa3af677966347a749d
Grigori Fursin <grigori.fursin@cTuning.org>
2017-01-21 22:25:18 +0100

    Added mingw downloaded to compile OpenBLAS on Windows (for Caffe for example)

---
+++ package/compiler-mingw-w64-gcc-download/process.bat
@@ -0,0 +1,29 @@
+@echo off
+
+rem
+rem Installation script for CK packages.
+rem
+rem See CK LICENSE.txt for licensing details.
+rem See CK Copyright.txt for copyright details.
+rem
+rem Developer(s): Grigori Fursin, 2015
+rem
+
+rem PACKAGE_DIR
+rem INSTALL_DIR
+
+echo NOTE: do not forget to select x86_64 version instead of i686 ...
+
+cd %PACKAGE_DIR%
+call mingw-w64-install.exe
+
+if %errorlevel% neq 0 (
+ echo.
+ echo Failed installing package!
+ goto err
+)
+
+exit /b 0
+
+:err
+exit /b 1
