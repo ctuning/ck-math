c51a29644b85d6049fb9258876f21abb35993b59
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-03-11 15:54:18 +0100

    fixing cltune and clblast for Android ...

---
+++ package/tool-cltune-master-universal/scripts.android/install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem
+rem Extra installation script
+rem
+rem See CK LICENSE.txt for licensing details.
+rem See CK Copyright.txt for copyright details.
+rem
+rem Developer(s): Grigori Fursin, 2016-2017
+rem
+
+set CK_CMAKE_EXTRA=%CK_CMAKE_EXTRA% ^
+ -DOPENCL_LIBRARIES:FILEPATH="%CK_ENV_LIB_OPENCL_LIB%\%CK_ENV_LIB_OPENCL_DYNAMIC_NAME%" ^
+ -DOPENCL_INCLUDE_DIRS:PATH="%CK_ENV_LIB_OPENCL_INCLUDE%" ^
+ -DSAMPLES=ON
+
+exit /b 0
---
+++ package/tool-cltune-master-universal/scripts.linux/install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#
+# Extra Installation script
+#
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, grigori.fursin@cTuning.org, 2017
+#
+
+export CK_CXX_FLAGS_FOR_CMAKE="${CK_CXX_FLAGS_FOR_CMAKE} -std=c++11"
+
+export CK_CMAKE_EXTRA="${CK_CMAKE_EXTRA} \
+ -DOPENCL_LIBRARIES:FILEPATH=${CK_ENV_LIB_OPENCL_LIB}/${CK_ENV_LIB_OPENCL_DYNAMIC_NAME} \
+ -DOPENCL_INCLUDE_DIRS:PATH=${CK_ENV_LIB_OPENCL_INCLUDE} \
+ -DSAMPLES=ON"
+
+return 0
