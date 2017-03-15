2bdb80b63b7e5c40ad5e77ae87b50661bfe459da
Anton Lokhmotov <anton@dividiti.com>
2017-03-15 18:28:46 +0000

    CLBlast Android scripts and patch (minor updates).

--- package/lib-clblast-master-universal-tune/scripts.android/install.bat
+++ package/lib-clblast-master-universal-tune/scripts.android/install.bat
@@ -1,17 +1,22 @@
 @echo off
 
 rem
-rem Extra installation script
+rem Installation script.
 rem
 rem See CK LICENSE.txt for licensing details.
-rem See CK Copyright.txt for copyright details.
+rem See CK COPYRIGHT.txt for copyright details.
 rem
-rem Developer(s): Grigori Fursin, 2016-2017
+rem Developer(s):
+rem - Grigori Fursin, grigori@dividiti.com, 2016-2017
+rem - Anton Lokhmotov, anton@dividiti.com, 2017
 rem
 
 set CK_CMAKE_EXTRA=%CK_CMAKE_EXTRA% ^
  -DOPENCL_ROOT="%CK_ENV_LIB_OPENCL%" ^
  -DOPENCL_LIBRARIES="%CK_ENV_LIB_OPENCL_LIB%\libOpenCL.so" ^
- -DOPENCL_INCLUDE_DIRS="%CK_ENV_LIB_OPENCL_INCLUDE%"
+ -DOPENCL_INCLUDE_DIRS="%CK_ENV_LIB_OPENCL_INCLUDE%" ^
+ -DTUNERS -DCLTUNE_ROOT="%CK_ENV_TOOL_CLTUNE%" ^
+ -DSAMPLES=ON ^
+ -DANDROID=ON"
 
 exit /b 0
--- package/lib-clblast-master-universal-tune/scripts.android/install.sh
+++ package/lib-clblast-master-universal-tune/scripts.android/install.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
 #
-# Extra Installation script
+# Installation script.
 #
 # See CK LICENSE.txt for licensing details.
 # See CK COPYRIGHT.txt for copyright details.
 #
 # Developer(s):
 # - Grigori Fursin, grigori.fursin@cTuning.org, 2017
+# - Anton Lokhmotov, anton@dividiti.com, 2017
 #
 
 export CK_CMAKE_EXTRA="${CK_CMAKE_EXTRA} \
  -DOPENCL_ROOT=${CK_ENV_LIB_OPENCL} \
  -DOPENCL_LIBRARIES=${CK_ENV_LIB_OPENCL_LIB}/libOpenCL.so \
  -DOPENCL_INCLUDE_DIRS=${CK_ENV_LIB_OPENCL_INCLUDE} \
- -DTUNERS=ON \
- -DCLTUNE_ROOT=${CK_ENV_TOOL_CLTUNE} \
- -DSAMPLES=ON"
+ -DTUNERS=ON -DCLTUNE_ROOT=${CK_ENV_TOOL_CLTUNE} \
+ -DSAMPLES=ON \
+ -DANDROID=ON"
 
 return 0
