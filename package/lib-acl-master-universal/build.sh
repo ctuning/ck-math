bb4e1035146cef5173ac04d7c6db334e57d211bc
Anton Lokhmotov <anton@dividiti.com>
2017-07-17 12:07:21 +0100

    Cosmetic changes to build script.

--- package/lib-acl-master-universal/build.sh
+++ package/lib-acl-master-universal/build.sh
@@ -7,7 +7,9 @@
 # See CK LICENSE.txt for licensing details.
 # See CK COPYRIGHT.txt for copyright details.
 #
-# Developer(s): Grigori Fursin, 2015
+# Developer(s):
+# - Grigori Fursin, 2015-2017
+# - Anton Lokhmotov, 2017
 #
 
 
@@ -16,7 +18,7 @@
 mkdir -p ../install
 mkdir -p ../install/lib
 
-mkdir obj
+mkdir -p obj
 cd obj
 
 # ******************************************************************************
@@ -34,7 +36,7 @@ echo ""
 ${CK_CXX} ${CK_COMPILER_FLAGS_OBLIGATORY} ${CK_FLAGS_STATIC_LIB} ${CK_FLAGS_CREATE_OBJ} ${CK_CXXFLAGS} ${CK_SRC_FILES} ${CK_FLAG_PREFIX_INCLUDE}..
 if [ "${?}" != "0" ] ; then
   echo ""
-  echo "Building failed!"
+  echo "Building static library failed!"
   exit 1
 fi
 
@@ -55,7 +57,7 @@ if [ "${?}" != "0" ] ; then
   exit 1
 fi
 
-# *******************************************************************************************************************************
+# ******************************************************************************
 
 CK_TARGET_FILE_D=${CK_TARGET_LIB}${CK_DLL_EXT}
 if [ "${CK_BARE_METAL}" != "on" ] ; then
@@ -68,20 +70,20 @@ if [ "${CK_BARE_METAL}" != "on" ] ; then
   echo "${CK_CXX} ${CK_COMPILER_FLAGS_OBLIGATORY} ${CK_FLAGS_DLL} ${CK_CXXFLAGS} ${CK_SRC_FILES} ${CK_FLAG_PREFIX_INCLUDE}.. ${CK_FLAGS_OUTPUT}${CK_TARGET_FILE_D} ${CK_FLAGS_DLL_EXTRA} ${CK_LD_FLAGS_MISC} ${CK_LD_FLAGS_EXTRA} ${CK_LFLAGS}"
   ${CK_CXX} ${CK_COMPILER_FLAGS_OBLIGATORY} ${CK_FLAGS_DLL} ${CK_CXXFLAGS} ${CK_SRC_FILES} ${CK_FLAG_PREFIX_INCLUDE}.. ${CK_FLAGS_OUTPUT}${CK_TARGET_FILE_D} ${CK_FLAGS_DLL_EXTRA} ${CK_LD_FLAGS_MISC} ${CK_LD_FLAGS_EXTRA} ${CK_LFLAGS}
   if [ "${?}" != "0" ] ; then
-   echo ""
-   echo "Building dynamic library failed!"
-   exit 1
+    echo ""
+    echo "Building dynamic library failed!"
+    exit 1
   fi
 
   cp -f $CK_TARGET_FILE_D ../../install/lib
   if [ "${?}" != "0" ] ; then
-   echo ""
-   echo "Copying dynamic library failed!"
-   exit 1
+    echo ""
+    echo "Copying dynamic library failed!"
+    exit 1
   fi
 fi
 
-# *******************************************************************************************************************************
+# ******************************************************************************
 
 echo ""
 echo "Copying include files ..."
