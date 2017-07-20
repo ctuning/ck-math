e439edf0fb85385dc2b868d09a7e872096c38baf
Anton Lokhmotov <anton@dividiti.com>
2017-07-20 15:42:25 +0100

    Add dependency on 227x227 ppm images.
    FIXME: Path to ppm utils is hardcoded.

--- program/acl-alexnet-neon/alexnet.cpp
+++ program/acl-alexnet-neon/alexnet.cpp
@@ -37,18 +37,42 @@
 #include "arm_compute/runtime/Tensor.h"
 #include "arm_compute/runtime/TensorAllocator.h"
 
-// Headers under ${CK_ENV_LIB_ACL_INCLUDE_TESTS}.
-#include "NEON/Helper.h"
-#include "NEON/NEAccessor.h"
-#include "TensorLibrary.h"
-#include "model_objects/AlexNet.h"
+// Headers under ${CK_ENV_LIB_ACL_SRC}.
+#include "tests/NEON/Helper.h"
+#include "tests/NEON/NEAccessor.h"
+#include "tests/TensorLibrary.h"
+#include "tests/model_objects/AlexNet.h"
+#include "utils/Utils.h"
 
 using namespace arm_compute;
+using namespace arm_compute::utils;
 using namespace arm_compute::test;
 using namespace arm_compute::test::neon;
 
 int main(int argc, char* argv[])
 {
+    // Init input image.
+    PPMLoader ppm;
+    Image image;
+    if (argc < 2)
+    {
+        // Print help.
+        std::cout << "Usage: " << argv[0] << " [input.ppm]\n\n";
+        std::cout << "No input provided, creating a dummy 227x227 image ...\n";
+        // Initialize the dimensions and format.
+        image.allocator()->init(TensorInfo(227, 227, Format::U8));
+    }
+    else
+    {
+        const char* image_path = argv[1];
+        std::cout << "Opening \'" << image_path << "\' ...\n\n";
+        ppm.open(image_path);
+        // Initialize the format.
+        ppm.init_image(image, Format::U8);
+    }
+    image.allocator()->allocate();
+
+    // Init AlexNet model.
     const unsigned int batches            = static_cast<unsigned int>(1);
     const bool         weights_transposed = true;
 
