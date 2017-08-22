53c6e827f4a3f5f8d889ed935f30c4764869d834
flavio <flavio@dividiti.com>
2017-08-22 16:14:44 +0000

    Add kerenl to tune

--- program/acl-softmax-opencl-tuner/softmax.c
+++ program/acl-softmax-opencl-tuner/softmax.c
@@ -2,13 +2,13 @@
 #include <xopenme.h>
 #endif
 
-//#include <arm_compute/runtime/NEON/NEFunctions.h>
+#include <arm_compute/runtime/NEON/NEFunctions.h>
 
 #define ARM_COMPUTE_CL /* So that OpenCL exceptions get caught too */
 #include "arm_compute/core/Types.h"
 #include "arm_compute/runtime/CL/CLFunctions.h"
 #include "arm_compute/runtime/CL/CLScheduler.h"
-//#include "tests/Utils.h"
+#include "tests/Utils.h"
 
 
 #include <arm_compute/core/Helpers.h>
@@ -65,18 +65,22 @@ int main( int argc, char *argv[] )
   for(unsigned int b = 0; b < bsize; b++){
      for(unsigned int h = 0; h < height; h++){
        for(unsigned int w = 0; w < width; w++){
-         float r = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/MYMAX));
+   //      float r = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/MYMAX));
          src_data[b * (width * height) + h * width + w] = static_cast<float>(100 * b + 10 * h + w); //replace with random fixed seed value
        }
      }
    }
-  auto kernel_file = std::vector<std::string>{"softmax_layer.cl"};
+  auto kernel_file = std::vector<std::string>{"/home/flavio/CK_REPOS/ck-math/program/acl-softmax-opencl-tuner/softmax_layer2.cl"};
   cltune::Tuner tuner(size_t{0}, size_t{0});
-  
-
-// end data init 
-
-
+  const auto id = tuner.AddKernel(kernel_file, "softmax_layer_max",{width,height,bsize} , {1});  
+  tuner.AddParameter(id, "GROUP_SIZE", {1, 2, 4, 8, 16, 32});
+  tuner.MulLocalSize(id, {"GROUP_SIZE"});
+  //tuner.AddArgumentInput(src_data);
+  //tuner.AddArgumentInput(dst_data);
+  //tuner.AddArgumentScalar(static_cast<int>(width));
+  //tuner.SetNumRuns(2);
+  //tuner.Tune();
+  //tuner.PrintToScreen();
 // OpenCL init
   //CLScheduler::get().default_init();
 
@@ -90,14 +94,6 @@ int main( int argc, char *argv[] )
   //FILL TENSORS... easiest way is: create an iteretor 
   Window input_window;
   input_window.use_tensor_dimensions(ATensor.info());
-  if ((width*height*bsize) < 16){
-   std::cout << " Dimensions of the input's iterator:\n";
-   std::cout << " X = [start=" << input_window.x().start() << ", end=" << input_window.x().end() << ", step=" << input_window.x().step() << "]\n";
-   std::cout << " Y = [start=" << input_window.y().start() << ", end=" << input_window.y().end() << ", step=" << input_window.y().step() << "]\n";
-   std::cout << " Z = [start=" << input_window.z().start() << ", end=" << input_window.z().end() << ", step=" << input_window.z().step() << "]\n";
-  }
-  CLSoftmaxLayer softmax;
-  softmax.configure(&ATensor,&OTensor);
 
   //Data in/out
   ATensor.allocator()->allocate();
@@ -106,19 +102,22 @@ int main( int argc, char *argv[] )
   ATensor.map();
   Iterator input_it(&ATensor, input_window);
   execute_window_loop(input_window, [&](const Coordinates & id){
-#ifdef PRINT
-    std::cout << "Setting item [" << id.x() << "," << id.y() << "," << id.z() << "]\n";
-#endif
     *reinterpret_cast<float *>(input_it.ptr()) = src_data[id.z() * (width * height) + id.y() * width + id.x()];
   },input_it);
   
   ATensor.unmap();
-//Execution
-  //softmax.run();
-  //CLScheduler::get().sync();
+
+
+  tuner.AddArgumentInput(ATensor);
+  tuner.AddArgumentInput(OTensor);
+  tuner.AddArgumentScalar(static_cast<int>(width));
+
+
+
+
 
 //Get output
-  Window output_window;
+/*  Window output_window;
   output_window.use_tensor_dimensions(OTensor.info());
 
   OTensor.map();
@@ -131,13 +130,13 @@ int main( int argc, char *argv[] )
     memcpy(dst_data + id.z() * (width * height) + id.y() * width, output_it.ptr(), width * sizeof(float));
   }, output_it);
   OTensor.unmap();
-
+*/
 
 
 // Compute time
-  secs += (after.tv_sec - before.tv_sec) + (after.tv_usec - before.tv_usec)/1000000.0;
-  std::cout << "Softmax[time]= " << secs; 
-  double avg_time = secs;
+//  secs += (after.tv_sec - before.tv_sec) + (after.tv_usec - before.tv_usec)/1000000.0;
+//  std::cout << "Softmax[time]= " << secs; 
+//  double avg_time = secs;
     
   delete[] src_data;
   delete[] dst_data;
