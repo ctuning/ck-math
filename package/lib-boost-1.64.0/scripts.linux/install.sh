a2100d33cdc3d1d1e943c3638d82d631c960ba95
Anton Lokhmotov <anton@dividiti.com>
2017-08-29 12:34:02 +0100

    Create dir at BOOST_BUILD_PATH before writing user-config.jam there.

--- package/lib-boost-1.64.0/scripts.linux/install.sh
+++ package/lib-boost-1.64.0/scripts.linux/install.sh
@@ -25,11 +25,12 @@ echo "Building Boost (can take a long time) ..."
 
 TOOLCHAIN=gcc
 if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
-  TOOLCHAIN=$CK_COMPILER_TOOLCHAIN_NAME
+  TOOLCHAIN=${CK_COMPILER_TOOLCHAIN_NAME}
 fi
 
-export BOOST_BUILD_PATH=$INSTALL_DIR/install
-echo "using ${TOOLCHAIN} : : ${CK_CXX} -fPIC ${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
+export BOOST_BUILD_PATH=${INSTALL_DIR}/install
+mkdir -p ${BOOST_BUILD_PATH}
+echo "using ${TOOLCHAIN} : : ${CK_CXX} -fPIC ${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > ${BOOST_BUILD_PATH}/user-config.jam
 
 ./b2 install -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} toolset=${TOOLCHAIN} address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH}
 
