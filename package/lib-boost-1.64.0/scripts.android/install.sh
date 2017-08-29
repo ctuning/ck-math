8da291bd1d132a4f1d2a66b97d3bf50349d06b48
Anton Lokhmotov <anton@dividiti.com>
2017-08-29 12:45:54 +0100

    Create dir at BOOST_BUILD_PATH for Android too.

--- package/lib-boost-1.64.0/scripts.android/install.sh
+++ package/lib-boost-1.64.0/scripts.android/install.sh
@@ -8,18 +8,18 @@
 #
 # Developer(s):
 # - Grigori Fursin, 2016-2017
-# - Anton Lokhmtoov, 2017
+# - Anton Lokhmotov, 2017
 #
 
 #  Check extra stuff
 
 EXTRA_FLAGS=""
 if [ "${CK_ARMEABI_V7A}" == "ON" ] ; then
-  EXTRA_FLAGS="$EXTRA_FLAGS -mfpu=neon"
+  EXTRA_FLAGS="${EXTRA_FLAGS} -mfpu=neon"
 fi
 
 if [ "${CK_ARMEABI_V7A_HARD}" == "ON" ] ; then
-  EXTRA_FLAGS="$EXTRA_FLAGS -mfpu=vfpv3"
+  EXTRA_FLAGS="${EXTRA_FLAGS} -mfpu=vfpv3"
 fi
 
 ############################################################
@@ -42,11 +42,12 @@ mkdir ${INSTALL_DIR}/install
 
 TOOLCHAIN=gcc
 if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
-  TOOLCHAIN=$CK_COMPILER_TOOLCHAIN_NAME
+  TOOLCHAIN=${CK_COMPILER_TOOLCHAIN_NAME}
 fi
 
 export BOOST_BUILD_PATH=${INSTALL_DIR}/install
-echo "using ${TOOLCHAIN} : arm : ${CK_CXX} ${CK_CXX_FLAGS_FOR_CMAKE} ${CK_CXX_FLAGS_ANDROID_TYPICAL} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
+mkdir -p ${BOOST_BUILD_PATH}
+echo "using ${TOOLCHAIN} : arm : ${CK_CXX} ${CK_CXX_FLAGS_FOR_CMAKE} ${CK_CXX_FLAGS_ANDROID_TYPICAL} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > ${BOOST_BUILD_PATH}/user-config.jam
 
 ./b2 install toolset=${TOOLCHAIN}-arm target-os=android -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} link=static --without-mpi address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH}
 # Ignore exit since some libs are not supported for Android ...
