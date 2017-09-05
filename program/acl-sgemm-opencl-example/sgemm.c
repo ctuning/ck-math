9fc75459c9a945c35b7a28aa4e3fcc81406a520a
Anton Lokhmotov <anton@dividiti.com>
2017-09-05 01:08:08 +0000

    Make acl-sgemm-opencl-example compile with lib-acl-opencl-17.06.

--- program/acl-sgemm-opencl-example/sgemm.c
+++ program/acl-sgemm-opencl-example/sgemm.c
@@ -2,15 +2,12 @@
 #include <xopenme.h>
 #endif
 
-//#include <arm_compute/runtime/NEON/NEFunctions.h>
-
 #define ARM_COMPUTE_CL /* So that OpenCL exceptions get caught too */
+
 #include "arm_compute/core/Types.h"
 #include "arm_compute/runtime/CL/CLFunctions.h"
 #include "arm_compute/runtime/CL/CLScheduler.h"
-#include "test_helpers/Utils.h"
-
-
+#include "arm_compute/tests/Utils.h"
 
 #include <arm_compute/core/Helpers.h>
 #include <arm_compute/core/ITensor.h>
@@ -26,16 +23,14 @@
 #include <cerrno>
 #include <iomanip>
 #include <string>
-#include <sys/time.h> 
+#include <sys/time.h>
+
 using namespace arm_compute;
-using namespace test_helpers;
+using namespace test;
+
 # define MYTIMER2 struct timeval
 static MYTIMER2 before, after;
 static double secs;
-
-
-
-
 
 int main( int argc, char *argv[] )
 {
@@ -44,17 +39,14 @@ int main( int argc, char *argv[] )
   long runs_max=10;
   int ct_return=0;
 
-// MM MN and MK come from compiler options... se .cm/meta
-
+  // (MM, MN, MK) come from compiler options (see .cm/meta)
   unsigned int m=MM;
   unsigned int n=MN;
   unsigned int k=MK;
-  if (getenv("CK_CLBLAST_MSIZE")!=NULL)  m=atol(getenv("CK_CLBLAST_MSIZE"));
-  if (getenv("CK_CLBLAST_NSIZE")!=NULL)  n=atol(getenv("CK_CLBLAST_NSIZE"));
-  if (getenv("CK_CLBLAST_KSIZE")!=NULL)  k=atol(getenv("CK_CLBLAST_KSIZE"));
-  if (getenv("CK_CLBLAST_ITERATIONS")!=NULL)  runs_max=atol(getenv("CK_CLBLAST_ITERATIONS"));
-
-  
+  if (getenv("CK_CLBLAST_MSIZE")!=NULL) m=atol(getenv("CK_CLBLAST_MSIZE"));
+  if (getenv("CK_CLBLAST_NSIZE")!=NULL) n=atol(getenv("CK_CLBLAST_NSIZE"));
+  if (getenv("CK_CLBLAST_KSIZE")!=NULL) k=atol(getenv("CK_CLBLAST_KSIZE"));
+  if (getenv("CK_CLBLAST_ITERATIONS")!=NULL) runs_max=atol(getenv("CK_CLBLAST_ITERATIONS"));
 
   const TensorShape AShape(k,m);
   const TensorShape BShape(n,k);
@@ -68,42 +60,36 @@ int main( int argc, char *argv[] )
   ATensor.allocator()->init(TensorInfo(AShape,Format::F32));
   BTensor.allocator()->init(TensorInfo(BShape,Format::F32));
   OTensor.allocator()->init(TensorInfo(OShape,Format::F32));
- 
+
   CLGEMM gemm;
   gemm.configure(&ATensor, &BTensor, NULL, &OTensor, 2.0f, 2.0f);
-//  gemm.configure(A, B, NULL, O, 1.0, 0.0);
 
-
-
-  //gemm.configure(&A,&B,NULL,&O,1.0,1.0);
   ATensor.allocator()->allocate();
   BTensor.allocator()->allocate();
   OTensor.allocator()->allocate();
   CLScheduler::get().default_init();
 
 //  if (getenv("RUNS")!=NULL) runs_max=atol(getenv("RUNS"));
-  for (r=0; r< runs_max; r++){
-    gettimeofday(&before, NULL); 
+  for (r=0; r< runs_max; r++) {
+    gettimeofday(&before, NULL);
     gemm.run();
     CLScheduler::get().sync();
     gettimeofday(&after, NULL);
     printf("ROUND %d = %lf\n",r, ((after.tv_sec - before.tv_sec) + (after.tv_usec - before.tv_usec)/1000000.0));
     secs += (after.tv_sec - before.tv_sec) + (after.tv_usec - before.tv_usec)/1000000.0;
-
   }
   double avg_time = secs / runs_max;
   double ops=m*n*k*2/avg_time;
   double gops= ops/(1000000000);
-  printf("M = %u\nN = %u\nK = %u\n",m,n,k);
-  printf("AVG = %lf\nREPETITIONS = %lu\n",avg_time, runs_max);
+  printf("M = %u\nN = %u\nK = %u\n", m, n, k);
+  printf("AVG = %lf\nREPETITIONS = %lu\n", avg_time, runs_max);
   printf("GFLOPS = %lf\n", gops);
   printf("STATUS = %d\n", 0);
 
   printf("------------- CLBLAST-STYLE_OUTPUT\n");
-  printf("m = %u\nn = %u\nk = %u\n",m,n,k);
-  printf("ms_1 = %lf\n",avg_time*1000);
+  printf("m = %u\nn = %u\nk = %u\n", m, n, k);
+  printf("ms_1 = %lf\n", avg_time*1000);
   printf("GFLOPS_1 = %lf\n", gops);
-  
 
   return 0;
 }
