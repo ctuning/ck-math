bd3e7b85b712ccdf1819482112258b13f33c8389
Anton Lokhmotov <anton@dividiti.com>
2017-10-31 12:05:09 +0000

    Update non-arm64 armcl validation similar to arm64.

--- program/validation-armcl-opencl/ck-run-validation.sh
+++ program/validation-armcl-opencl/ck-run-validation.sh
@@ -2,25 +2,42 @@
 
 # Collective Knowledge (program)
 #
-# See CK LICENSE.txt for licensing details
-# See CK COPYRIGHT.txt for copyright details
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
 #
-# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org
-#            Flavio Vella, dividiti
-#
+# Developers:
+# - Grigori Fursin, Grigori.Fursin@cTuning.org
+# - Flavio Vella, flavio@dividiti.com
+# - Anton Lokhmotov, anton@dividiti.com
+
+export WORKING_DIR=`pwd`
+
+#------------------------------------------------------------------------------
+echo ""
+echo "Copying OpenCL kernels from '${CK_ENV_LIB_ARMCL_CL_KERNELS}' to '${WORKING_DIR}' ..."
+
+if [ ! -d "${CK_ENV_LIB_ARMCL_CL_KERNELS}" ]; then
+  echo "ERROR: Directory '${CK_ENV_LIB_ARMCL_CL_KERNELS}' with OpenCL kernels does not exist!"
+  exit 1
+fi
+
+rm -rf ${WORKING_DIR}/cl_kernels
+cp -r ${CK_ENV_LIB_ARMCL_CL_KERNELS} ${WORKING_DIR}
+
+#------------------------------------------------------------------------------
+echo ""
+echo "Running original ArmCL validation with --seed=${SEED} and --filter='${FILTER}' ..."
 
 echo ""
-echo "Copying CL kernels ..."
+
+export LD_LIBRARY_PATH=${CK_ENV_LIB_ARMCL_SRC}/build:${LD_LIBRARY_PATH}
+
+./arm_compute_validation $1 --error-on-missing-assets \
+  --seed=${SEED} --filter=${FILTER}
+
 echo ""
 
-rm -rf cl_kernels
-mkdir cl_kernels
-cp ${CK_ENV_LIB_ARMCL_CL_KERNELS}/* cl_kernels
+# --env.FILTER='CL/SoftmaxLayer/Float/FP\\d\\d/RunSmall@Shape=633x11x3x5'
 
-echo ""
-echo "Running original ARMCL validation ..."
-echo ""
-
-./arm_compute_validation $1 --filter=${FILTER}
-
+#------------------------------------------------------------------------------
 return 0
