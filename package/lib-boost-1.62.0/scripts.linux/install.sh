40998351987ca572ee34af5bf8547eb8ecb85aa2
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-02-20 17:01:53 +0000

    bugfix: pass the correct toolset both to the bootstrap and b2
    Also renamed TOOLCHAIN to TOOLSET to be consistent with Boost
    (which is currently the only consumer of the CK_COMPILER_TOOLCHAIN_NAME variable)

--- package/lib-boost-1.62.0/scripts.linux/install.sh
+++ package/lib-boost-1.62.0/scripts.linux/install.sh
@@ -11,9 +11,15 @@
 # - Anton Lokhmotov, 2017
 #
 
+if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
+  TOOLSET=$CK_COMPILER_TOOLCHAIN_NAME
+else
+  TOOLSET=gcc
+fi
+
 ############################################################
 cd ${INSTALL_DIR}/${PACKAGE_SUB_DIR1}
-./bootstrap.sh
+./bootstrap.sh --with-toolset=${TOOLSET}
 if [ "${?}" != "0" ] ; then
   echo "Error: cmake failed!"
   exit 1
@@ -23,15 +29,10 @@ fi
 echo ""
 echo "Building Boost (can take a long time) ..."
 
-TOOLCHAIN=gcc
-if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
-  TOOLCHAIN=$CK_COMPILER_TOOLCHAIN_NAME
-fi
-
 export BOOST_BUILD_PATH=$INSTALL_DIR/install
-echo "using ${TOOLCHAIN} : : ${CK_CXX} -fPIC ${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
+echo "using ${TOOLSET} : : ${CK_CXX} -fPIC ${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
 
-./b2 install -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} toolset=${TOOLCHAIN} address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH} ${BOOST_B2_FLAGS}
+./b2 install -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} toolset=${TOOLSET} address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH} ${BOOST_B2_FLAGS}
 
 if [ "${?}" != "0" ] ; then
   echo "Error: b2 make failed!"
