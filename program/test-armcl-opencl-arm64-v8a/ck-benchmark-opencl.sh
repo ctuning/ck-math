399840492dac5752f1a61890178242fb629c06a3
Anton Lokhmotov <anton@dividiti.com>
2018-02-21 16:00:20 +0000

    Add cmds to benchmark OpenCL examples.

---
+++ program/test-armcl-opencl-arm64-v8a/ck-benchmark-opencl.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# Collective Knowledge (CK).
+#
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# Developers:
+# - Grigori Fursin, Grigori.Fursin@cTuning.org
+# - Flavio Vella, flavio@dividiti.com
+# - Anton Lokhmotov, anton@dividiti.com
+#
+
+export WORKING_DIR=`pwd`
+
+#------------------------------------------------------------------------------
+echo ""
+echo "Copying OpenCL kernels from '${CK_ENV_LIB_ARMCL_CL_KERNELS}' to '${WORKING_DIR}' ..."
+
+if [ ! -d "${CK_ENV_LIB_ARMCL_CL_KERNELS}" ]; then
+  echo "ERROR: Directory '${CK_ENV_LIB_ARMCL_CL_KERNELS}' with OpenCL kernels does not exist!"
+  exit 1
+fi
+
+rm -rf ${WORKING_DIR}/cl_kernels
+cp -r ${CK_ENV_LIB_ARMCL_CL_KERNELS} ${WORKING_DIR}
+
+#------------------------------------------------------------------------------
+echo ""
+echo "Benchmarking '${EXECUTABLE}' with args '${ARGS}' (0 - Neon, 1 - OpenCL) ..."
+
+export LD_LIBRARY_PATH=${CK_ENV_LIB_ARMCL_SRC}/build:${LD_LIBRARY_PATH}
+
+${EXECUTABLE} --instruments=${INSTRUMENTS} --example_args=${ARGS} --iterations=${ITERATIONS}
+
+echo ""
+
+#------------------------------------------------------------------------------
+return 0
