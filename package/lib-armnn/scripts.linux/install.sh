1c186d947541ceaf42cbd91120a421f9e7030ba5
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-02-08 19:42:48 +0000

    optionally depend on ArmCL+OpenCL; assume ArmCL directory structure generated by scons

--- package/lib-armnn/scripts.linux/install.sh
+++ package/lib-armnn/scripts.linux/install.sh
@@ -13,17 +13,17 @@
 #   PACKAGE_SUB_DIR = "src"
 
 ARMNN_SOURCE_DIR=$INSTALL_DIR/$PACKAGE_SUB_DIR
-ARMCL_SOURCE_DIR=$INSTALL_DIR/ComputeLibrary/src
-
-#ARMNN_BUILD_DIR=$ARMNN_SOURCE_DIR/build
 ARMNN_BUILD_DIR=$INSTALL_DIR/obj
 ARMNN_TARGET_DIR=$INSTALL_DIR/install
+
 TF_PB_DIR=$INSTALL_DIR/generated_tf_pb_files
 
 ############################################################
 echo ""
 echo "Building ArmNN package in $INSTALL_DIR ..."
 echo ""
+
+set
 
 ############################################################
 echo ""
@@ -37,15 +37,23 @@ ${ARMNN_SOURCE_DIR}/scripts/generate_tensorflow_protobuf.sh ${TF_PB_DIR} ${CK_EN
 
 ############################################################
 echo ""
-echo "Running cmake for ArmNN ..."
+echo "Running cmake for ArmNN with USE_OPENCL='${USE_OPENCL}' ..."
 echo ""
+
+if [ "$USE_OPENCL" == "ON" ] || [ "$USE_OPENCL" == "on" ] || [ "$USE_OPENCL" == "1" ]
+then
+    NUMERIC_USE_OPENCL=1
+else
+    NUMERIC_USE_OPENCL=0
+fi
 
 rm -rf "${ARMNN_BUILD_DIR}"
 mkdir ${ARMNN_BUILD_DIR}
 cd ${ARMNN_BUILD_DIR}
 cmake ${ARMNN_SOURCE_DIR} \
-    -DARMCOMPUTE_ROOT=${ARMCL_SOURCE_DIR} \
-    -DARMCOMPUTE_BUILD_DIR=${ARMCL_SOURCE_DIR}/build \
+    -DARMCOMPUTECL=${NUMERIC_USE_OPENCL} \
+    -DARMCOMPUTE_ROOT=${CK_ENV_LIB_ARMCL} \
+    -DARMCOMPUTE_BUILD_DIR=${CK_ENV_LIB_ARMCL}/build \
     -DBOOST_ROOT=$CK_ENV_LIB_BOOST \
     -DTF_GENERATED_SOURCES=${TF_PB_DIR} \
     -DBUILD_TF_PARSER=1 \
