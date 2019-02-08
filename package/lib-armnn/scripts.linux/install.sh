62c8933304f111e250088504b071c5ba93343e61
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-02-08 14:18:52 +0000

    using multithreaded make speeds up the building process considerably

--- package/lib-armnn/scripts.linux/install.sh
+++ package/lib-armnn/scripts.linux/install.sh
@@ -9,13 +9,14 @@
 #
 
 # for example,
-#   INSTALL_DIR     = "~/CK-TOOLS/lib-leveldb-1.20-llvm-5.0.0-macos-64"
-#   PACKAGE_SUB_DIR = "leveldb-1.20"
+#   INSTALL_DIR     = "lib-armnn-linux-64"
+#   PACKAGE_SUB_DIR = "src"
 
 ARMNN_SOURCE_DIR=$INSTALL_DIR/$PACKAGE_SUB_DIR
 ARMCL_SOURCE_DIR=$INSTALL_DIR/ComputeLibrary/src
 
-ARMNN_BUILD_DIR=$ARMNN_SOURCE_DIR/build
+#ARMNN_BUILD_DIR=$ARMNN_SOURCE_DIR/build
+ARMNN_BUILD_DIR=$INSTALL_DIR/obj
 ARMNN_TARGET_DIR=$INSTALL_DIR/install
 TF_PB_DIR=$INSTALL_DIR/generated_tf_pb_files
 
@@ -23,8 +24,6 @@ TF_PB_DIR=$INSTALL_DIR/generated_tf_pb_files
 echo ""
 echo "Building ArmNN package in $INSTALL_DIR ..."
 echo ""
-
-set
 
 ############################################################
 echo ""
@@ -44,7 +43,7 @@ echo ""
 rm -rf "${ARMNN_BUILD_DIR}"
 mkdir ${ARMNN_BUILD_DIR}
 cd ${ARMNN_BUILD_DIR}
-cmake .. \
+cmake ${ARMNN_SOURCE_DIR} \
     -DARMCOMPUTE_ROOT=${ARMCL_SOURCE_DIR} \
     -DARMCOMPUTE_BUILD_DIR=${ARMCL_SOURCE_DIR}/build \
     -DBOOST_ROOT=$CK_ENV_LIB_BOOST \
@@ -52,8 +51,4 @@ cmake .. \
     -DBUILD_TF_PARSER=1 \
     -DPROTOBUF_ROOT=$CK_ENV_LIB_PROTOBUF_HOST \
     -DCMAKE_INSTALL_PREFIX=${ARMNN_TARGET_DIR}
-
-make
-
-make install
 
