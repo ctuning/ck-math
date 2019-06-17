9ac0c5baeb635b4e77182466baabe5e00d81711c
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-06-17 15:53:17 +0100

    Cross-compile Boost for Android using a standalone-toolchain (controllable via BOOST_FOR_ARMNN variable)

--- package/lib-boost-1.62.0/scripts.android/install.sh
+++ package/lib-boost-1.62.0/scripts.android/install.sh
@@ -30,15 +30,18 @@ if [ "${?}" != "0" ] ; then
   exit 1
 fi
 
+export BOOST_BUILD_PATH=${INSTALL_DIR}/install
+
 ############################################################
 echo ""
 echo "Building Boost (can take a long time) ..."
 
-if [ -d ${INSTALL_DIR}/install ] ; then
-  rm -rf ${INSTALL_DIR}/install
+
+if [ -d "$BOOST_BUILD_PATH" ] ; then
+  rm -rf "$BOOST_BUILD_PATH"
 fi
 
-mkdir ${INSTALL_DIR}/install
+mkdir "$BOOST_BUILD_PATH"
 
 TOOLCHAIN=gcc
 if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
@@ -46,22 +49,30 @@ if [ "${CK_COMPILER_TOOLCHAIN_NAME}" != "" ] ; then
 fi
 
 BOOST_B2_LINK=""
-if [ "${BOOST_STATIC}" == "yes" ] && [ "${BOOST_SHARED}" == "yes" ]; then
-	BOOST_B2_LINK="link=static,shared"
-elif [ "${BOOST_STATIC}" == "yes" ]; then
+if [ "$BOOST_SHARED" == "yes" ]; then
+	BOOST_B2_LINK="link=static,shared hardcode-dll-paths=true dll-path=\"${BOOST_BUILD_PATH}/lib\""
+	#BOOST_B2_LINK="link=static,shared hardcode-dll-paths=false"
+elif [ "$BOOST_STATIC" == "yes" ]; then
 	BOOST_B2_LINK="link=static"
-elif [ "${BOOST_SHARED}" == "yes" ]; then
-	BOOST_B2_LINK="link=shared"
 fi
 
-export BOOST_BUILD_PATH=${INSTALL_DIR}/install
-echo "using ${TOOLCHAIN} : ${CK_ANDROID_NDK_ARCH} : ${CK_CXX} ${CK_COMPILER_FLAG_CPP14} -I${CK_ENV_LIB_STDCPP_INCLUDE} -I${CK_ENV_LIB_STDCPP_INCLUDE_EXTRA} ${CK_CXX_FLAGS_FOR_CMAKE} ${CK_CXX_FLAGS_ANDROID_TYPICAL} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
+if [ -n "$CK_ENV_STANDALONE_TOOLCHAIN_SET" ]; then
+#  echo "using clang : ${CK_ENV_STANDALONE_TOOLCHAIN_TARGET_ARCH} : ${CK_ENV_STANDALONE_TOOLCHAIN_ROOT}/bin/clang++ ;" > $BOOST_BUILD_PATH/user-config.jam
+#  TOOLSET=clang-${CK_ENV_STANDALONE_TOOLCHAIN_TARGET_ARCH}
+  echo "using clang : android : ${CK_ENV_STANDALONE_TOOLCHAIN_ROOT}/bin/clang++ ;" > $BOOST_BUILD_PATH/user-config.jam
+  TOOLSET=clang-android
+else
+  echo "using ${TOOLCHAIN} : ${CK_ANDROID_NDK_ARCH} : ${CK_CXX} ${CK_COMPILER_FLAG_CPP14} -I${CK_ENV_LIB_STDCPP_INCLUDE} -I${CK_ENV_LIB_STDCPP_INCLUDE_EXTRA} ${CK_CXX_FLAGS_FOR_CMAKE} ${CK_CXX_FLAGS_ANDROID_TYPICAL} ${EXTRA_FLAGS} -DNO_BZIP2 ;" > $BOOST_BUILD_PATH/user-config.jam
+  TOOLSET=${TOOLCHAIN}-${CK_ANDROID_NDK_ARCH}
+fi
 
 if [ "${BOOST_B2_FLAGS}" == "" ] ; then
   BOOST_B2_FLAGS=--without-mpi
 fi
 
-./b2 install -j${CK_HOST_CPU_NUMBER_OF_PROCESSORS} toolset=${TOOLCHAIN}-${CK_ANDROID_NDK_ARCH} target-os=android address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH} ${BOOST_B2_LINK} ${BOOST_B2_FLAGS}
+set
+
+./b2 install -j${CK_HOST_CPU_NUMBER_OF_PROCESSORS} toolset=${TOOLSET} target-os=android threading=multi address-model=${CK_TARGET_CPU_BITS} --prefix=${BOOST_BUILD_PATH} ${BOOST_B2_LINK} ${BOOST_B2_FLAGS}
 # Ignore exit since some libs are not supported for Android ...
 #if [ "${?}" != "0" ] ; then
 #  echo "Error: b2 make failed!"
