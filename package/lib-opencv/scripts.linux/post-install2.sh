266f7ab81ae5a3d6a8a6064cda938c8b5b5a7933
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-07-16 11:45:55 +0100

    bugfix: another attempt that may work for AWS

--- package/lib-opencv/scripts.linux/post-install2.sh
+++ package/lib-opencv/scripts.linux/post-install2.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Make sure we can reach our library via lib/ when it is actually in lib64/
+# Make sure we can reach our libraries via lib/ even if they were originally put into lib64/
 
-if [ -d "$INSTALL_DIR/install/lib64" ] && ! [ -d "$INSTALL_DIR/install/lib" ]; then
-    ln -s lib64 "$INSTALL_DIR/install/lib"
+if [ -d "$INSTALL_DIR/install/lib64" ]; then
+
+    if ! [ -d "$INSTALL_DIR/install/lib" ]; then
+        mkdir ${INSTALL_DIR}/install/lib
+    fi
+
+    cp -r ${INSTALL_DIR}/install/lib64/* ${INSTALL_DIR}/install/lib
 fi
